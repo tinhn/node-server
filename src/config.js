c09c98be3a81cb8003acfd9e8dccc36ca785c19f
tinh nguyen <tinhnguyen@imt-soft.com>
2019-01-20 17:31:02 +0700

    update config value

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 module.exports = {
     redis: {
-        host: '192.168.100.112',
+        host: '192.168.100.x',
         port: 6379
     },
     rabbit:{
-        host: '192.168.100.112',
+        host: '192.168.100.x',
         port: 5672,
-        user: 'dev',
-        password: 'dev',
-        queue_name: 'chat.message'
+        user: 'user',
+        password: 'password',
+        queue_name: '<your_queue_name>'
     },
     socketaddress: 8888,
     logtrackingdir:'chatlogs'
